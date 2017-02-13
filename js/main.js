526292a770ddef4262b1e69106733fde1e06279d
robben5566 <ccav31104@gmail.com>
2017-02-13 15:41:24 +0800

    v1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+     $('[data-toggle="tooltip"]').tooltip(); 
+    smoooth();
+});
+
+function smoooth() {
+        $(".navbar a, footer a[href='#home']").on('click', function(event){
+            
+            if(this.hash !== "") {
+                event.preventDefault();
+                
+                var hash = this.hash;
+                
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 500, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
+    }
